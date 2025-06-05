f4e48acdbf8ff39d80a68da1920003a3d9138828
Saroj <sarojsharmaa126@gmail.com>
2025-06-05 11:18:53 +0545

    added function to display report of resources

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-choice = input("what would you like?(espresso/latte/cappuccino):")
+from resources_db import machine_resources as resources
+def get_resources_report():
+    print(f"water:{resources.get('water',' ')}ml")
+    print(f"milk:{resources.get('water',' ')}ml")
+    print(f"coffee:{resources.get('water',' ')}gm")
+    print(f"money:{resources.get('water',' ')}ml")
+
 is_machine_on = True
 while is_machine_on:
     choice = input("what would you like?(espresso/latte/cappuccino):")
     if choice.lower()=="off":
         is_machine_on = False
+
+    elif choice.lower() == "report":
+        get_resources_report()
---
+++ resources_db.py
@@ -0,0 +1,6 @@
+machine_resources = {
+    "water":500,
+    "milk":400,
+    "coffee":300,
+    "money":0,
+}
