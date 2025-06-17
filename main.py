4639f4259ef5bf215182bf40e46059117b64eb45
Saroj Sharma <sarojsharmaa126@gmail.com>
2025-06-17 17:23:43 +0545

    added change

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from utils import get_resources_report, is_resources_sufficient,process_coins
-
+from utils import get_resources_report, is_resources_sufficient, process_coins
 
 is_machine_on = True
+
 while is_machine_on:
-    choice = input("what would you like?(espresso/latte/cappuccino):")
-    if choice.lower()=="off":
+    choice = input("what would you like?(espresso/latte/cappuccino): ")
+    if choice.lower() == "off":
         is_machine_on = False
 
     elif choice.lower() == "report":
--- resources_db.py
+++ resources_db.py
@@ -1,33 +1,33 @@
 machine_resources = {
-    "water":500,
-    "milk":300,
-    "coffee":100,
-    "money":0,
+    "water": 500,
+    "milk": 300,
+    "coffee": 100,
+    "money": 0
 }
 
 COFFEE_MENU = {
     "latte":{
-        "ingredients":{
-        "water":200,
-        "milk":150,
-        "coffee":24
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24
         },
-        "money":2.5      
+        "money": 2.5      
     },
     "espresso":{
-        "ingredients":{
-        "water":50,
-        "milk":0,
-        "coffee":18
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffee": 18
         },
-        "money":1.5    
+        "money": 1.5    
     },
     "cappuccino":{
-        "ingredients":{
-        "water":300,
-        "milk":125,
-        "coffee":30
+        "ingredients": {
+            "water": 300,
+            "milk": 125,
+            "coffee": 30
         },
-        "money":3.5   
+        "money": 3.5   
     }
 }
